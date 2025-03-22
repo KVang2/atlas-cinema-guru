159b84c6cde7fa44588f594d0f8e1a49a37b8f78
kvang2 <kevinvaj88@hotmail.com>
2025-03-22 05:46:27 -0500

    test the log for secret

--- auth.ts
+++ auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
+console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
     brandColor: "#1ED2AF",
