48d16f6b02e5053158508f6c5e25465704be9810
kvang2 <kevinvaj88@hotmail.com>
2025-03-22 06:06:44 -0500

    test login page

--- app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-// This file simply redirects to the built-in login UI powered by `auth.ts`
-
-import { redirect } from "next/navigation";
-
-export default function LoginRedirectPage() {
-  redirect("/api/auth/signin");
-}
--- auth.ts
+++ auth.ts
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-
-console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
+import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
@@ -9,11 +7,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
-  providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID!,
-      clientSecret: process.env.AUTH_GITHUB_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
+  providers: [GitHub],
+  secret: process.env.AUTH_GITHUB_SECRET,
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+  },
 });
